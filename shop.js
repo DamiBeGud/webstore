efc34a506e5616d22bdfe7d5baa5b95f1819e41c
DamiBeGud <dami.ml1998@gmail.com>
2023-11-03 15:18:31 +0100

    Improvments to shop page

--- shop.js
+++ shop.js
@@ -1,14 +1,37 @@
 // clear filters
 
-const clearFiltersButton = document.getElementById('clear-filter')
-const checkBoxes = document.getElementsByClassName('checkbox')
+// const clearFiltersButton = document.getElementById('clear-filter')
+// const checkBoxes = document.getElementsByClassName('checkbox')
 
 
-clearFiltersButton.addEventListener('click', (e)=>{
-    e.preventDefault()
-    for(let i = 0; i<= 4;i++){
-        checkBoxes[i].checked = false
-    }
+// clearFiltersButton.addEventListener('click', (e)=>{
+//     e.preventDefault()
+//     for(let i = 0; i<= 4;i++){
+//         checkBoxes[i].checked = false
+//     }
+
+// })
+
+
+const closeCategores = document.getElementById('close')
+const openCategores = document.getElementById('open')
+
+closeCategores.addEventListener('click',()=>{
+    document.getElementById('categories').classList.add('conteiner-card-categories-close')
+    document.getElementById('categories').classList.remove('conteiner-card-categories-open')
+    document.getElementById('categories-wrapper').classList.add('hidden')
+    document.getElementById('categories-text').classList.remove('hidden')
+    openCategores.classList.remove('hidden')
+    closeCategores.classList.add('hidden')
 
 })
 
+
+openCategores.addEventListener('click',()=>{
+    document.getElementById('categories').classList.add('conteiner-card-categories-open')
+    document.getElementById('categories').classList.remove('conteiner-card-categories-close')
+    document.getElementById('categories-wrapper').classList.remove('hidden')
+    document.getElementById('categories-text').classList.add('hidden')
+    openCategores.classList.add('hidden')
+    closeCategores.classList.remove('hidden')
+})
